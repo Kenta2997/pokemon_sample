41b2e02b8f76a892bacc1e78816cbac3ff446e4e
kenta2997 <kenta.6934@gmail.com>
2022-09-18 18:32:33 +0900

    Step7まで

--- lib/main.dart
+++ lib/main.dart
@@ -1,36 +1,85 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import './poke_list_item.dart';
+import './settings.dart';
+import './models/theme_mode.dart';
 
-void main() {
-  runApp(const MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  final SharedPreferences pref = await SharedPreferences.getInstance();
+  final themeModeNotifier = ThemeModeNotifier(pref);
+  runApp(ChangeNotifierProvider(
+    create: (context) => themeModeNotifier,
+    child: const MyApp(),
+  ));
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({Key? key}) : super(key: key);
   @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Pokemon Flutter',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+    return Consumer<ThemeModeNotifier>(
+      builder: (context, mode, child) => MaterialApp(
+        title: 'Pokemon Flutter',
+        theme: ThemeData.light(),
+        darkTheme: ThemeData.dark(),
+        themeMode: mode.mode,
+        home: const TopPage(),
       ),
-      home: const MyHomePage(),
     );
   }
 }
 
-class MyHomePage extends StatelessWidget {
-  const MyHomePage({Key? key}) : super(key: key);
+class TopPage extends StatefulWidget {
+  const TopPage({Key? key}) : super(key: key);
+  @override
+  _TopPageState createState() => _TopPageState();
+}
+
+class _TopPageState extends State<TopPage> {
+  int currentbnb = 0;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Image.network(
-        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png",
-          height: 300,
-          width: 300,
-          // レスポンシブデザインを意識すべき
-        ),
+      body: SafeArea(
+        child: currentbnb == 0 ? const PokeList() : const Settings(),
       ),
+      bottomNavigationBar: BottomNavigationBar(
+        onTap: (index) => {
+          setState(
+                () => currentbnb = index,
+          )
+        },
+        currentIndex: currentbnb,
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.list),
+            label: 'home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.settings),
+            label: 'settings',
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class PokeList extends StatelessWidget {
+  const PokeList({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      padding: const EdgeInsets.symmetric(vertical: 4, horizontal: 16),
+      itemCount: 898,
+      itemBuilder: (context, index) => PokeListItem(index: index),
     );
   }
 }
